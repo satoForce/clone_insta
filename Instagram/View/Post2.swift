c288cdd44491999fd3507e35e27f624f8b61adc6
Valery Chan <valerychan.dev@gmail.com>
2023-10-10 11:02:43 -0500

    PR feedback

--- Instagram/View/Post2.swift
+++ Instagram/View/Post2.swift
@@ -70,7 +70,7 @@ struct Post2: View {
                     Text("perro1")
                       .fontWeight(.bold)
                     
-                    Text("Adiós")
+                    Text("Hola!!!")
                   }
                   .font(.caption)
                   
